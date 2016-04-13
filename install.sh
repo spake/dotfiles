4256615091f8e6e8dfae5a51f77e0007c89ded32
George Caley <george@caley.com.au>
2016-04-12 23:43:54 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+echo " * Checking for prerequisites"
+PREREQS=( stow curl git vim zsh )
+for prereq in ${PREREQS[@]}; do
+    if ! which -s $prereq; then
+        echo "   - ERROR: Couldn't find $prereq."
+        echo "     Make sure it's installed (and in your \$PATH) and try again."
+        exit 1
+    fi
+done
+
+# change into dotfiles directory
+cd "`dirname $0`"
+
+echo " * Stowing dotfiles"
+# stow dotfiles
+for f in *; do
+    if [ -d $f ]; then
+        echo "   - Stowing $f"
+        stow "--target=$HOME" $f
+    fi
+done
+
+# install antigen
+echo " * Checking for antigen"
+if ! [ -f ~/.antigen.zsh ]; then
+    echo "   - Installing antigen"
+    curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > "$HOME/.antigen.zsh"
+fi
+
+# install vundle
+echo " * Checking for Vundle"
+if ! [ -d ~/.vim ]; then
+    echo "   - Installing Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+
+echo " * Done!"
